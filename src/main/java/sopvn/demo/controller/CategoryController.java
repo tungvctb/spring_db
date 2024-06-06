7ccb5c41ad69ff8d9401acae0717e041eb01ac11
Nguyen Thanh Tung <ngthtunghhtb@gmail.com>
2024-06-06 11:26:30 +0700

    lam detail categ

--- src/main/java/sopvn/demo/controller/CategoryController.java
+++ src/main/java/sopvn/demo/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 //import ch.qos.logback.core.model.Model;
 import sopvn.demo.model.Category;
@@ -17,9 +18,15 @@ public class CategoryController {
 	@Autowired
 	CategoryRepository rep;
 	@GetMapping("/view")
-	public String viewMgr(Model model) {
+	public String viewMgr( Model model) {
 		List<Category> lsc = rep.Instance().findAll();
 		model.addAttribute("cates", lsc);
 		return "ad_layout/cate_mgr";
+	}
+	@GetMapping("/detail")
+	public String detail(@RequestParam("idc") int idc, Model model) {
+		Category category = rep.Instance().findById(idc);
+		model.addAttribute("category", category);
+		return "ad_layout/cate_detail";
 	}
 }
